c4958b44ccb72c03c662332e91b7523c2bd94bb3
yihan <yihan@360.cn>
2015-10-26 06:06:30 -0700

    add filename

--- csearch_main.py
+++ csearch_main.py
@@ -1,78 +1,91 @@
-from csearch import CSearch,SearchResult
+# coding=utf-8
+
+from csearch import CSearch, SearchResult
 import tornado.ioloop
 import tornado.web
 import os
 import time
 
+
 class MainHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.write("Hello, world")
+
+    def get(self):
+        self.write("Hello, world")
+
 
 class IndexHandler(tornado.web.RequestHandler):
-	def get(self):
-		regexp = self.get_argument('regexp',default='')
-		file_regexp = self.get_argument('file_regexp',default='')
-		if regexp:
-			cs = CSearch()
 
-			cs.search(regexp, file_regexp)
-			self.render("index.html",  rst_list=cs.rst_list)
-		else:
-			self.render("index.html",  rst_list=[])
+    def get(self):
+        regexp = self.get_argument('regexp', default='')
+        file_regexp = self.get_argument('file_regexp', default='')
+        if regexp:
+            cs = CSearch()
+
+            cs.search(regexp, file_regexp)
+            self.render("index.html",  rst_list=cs.rst_list)
+        else:
+            self.render("index.html",  rst_list=[])
+
 
 class AddHandler(tornado.web.RequestHandler):
-	def get_file_name(self, code_type):
-		_dir = self.settings["csearch_path"]
-		tag_file_name = self.settings["csearch_tag_file"]
-		tag_file_full_name = _dir + '/' + tag_file_name
 
-		full_file_name = _dir + '/' + code_type + '/' + str(time.time())
+    def get_file_name(self, code_type, filename):
+        if not filename:
+            filename = str(time.time())
+        _dir=self.settings["csearch_path"]
+        tag_file_name=self.settings["csearch_tag_file"]
+        tag_file_full_name=_dir + '/' + tag_file_name
 
-		return tag_file_full_name, full_file_name
+        full_file_name=_dir + '/' + code_type + '/' + filename
 
-	def post(self):		
-		_type = self.get_argument('type');
-		_tags = self.get_argument('tags');
-		_code = self.get_argument('code');
-		if not _code.strip():
-			self.render("index.html",  rst_list=[])
-			return
-		tag_file_full_name, full_file_name = self.get_file_name(_type)
-		print _type, _tags, _code, tag_file_full_name, full_file_name
+        return tag_file_full_name, full_file_name
 
-		with open(full_file_name,'aw') as file:
-			file.write('%s\n' %_code)
-			file.flush()
-		with open(tag_file_full_name,'a') as file:
-			file.writelines('%s-%s-%s\n' %(_type, _tags, full_file_name))
-			file.flush()
+    def post(self):
+        _type = self.get_argument('type')
+        _tags = self.get_argument('tags')
+        _code = self.get_argument('code')
+        _filename = self.get_argument('filename')
+        if not _code.strip():
+            self.render("index.html",  rst_list=[])
+            return
+        tag_file_full_name, full_file_name = self.get_file_name(
+            _type, _filename)
+        print _type, _tags, _code, _filename, tag_file_full_name, full_file_name
 
-		cs = CSearch()
-		cs.index()
-		self.write('It is ok')
+        with open(full_file_name, 'aw') as file:
+            file.write('%s\n' % _code)
+            file.flush()
+        with open(tag_file_full_name, 'a') as file:
+            file.writelines('%s-%s-%s\n' % (_type, _tags, full_file_name))
+            file.flush()
+
+        cs = CSearch()
+        cs.index()
+        self.write('It is ok')
+
 
 class FileHandler(tornado.web.RequestHandler):
-	def get(self):
-		file_name=self.get_argument("filename")
-		with open(file_name,'r') as file:
-			code = file.read()
-			#self.render('file.html',code=markdown2.markdown(`code`))
-			self.render('file.html',code=code)
+
+    def get(self):
+        file_name = self.get_argument("filename")
+        with open(file_name, 'r') as file:
+            code = file.read()
+            # self.render('file.html',code=markdown2.markdown(`code`))
+            self.render('file.html', code=code)
 
 
 settings = {
-	"template_path": os.path.join(os.path.dirname(__file__), "templates"),
-	"csearch_path" : '/u01/cdata',
-	"csearch_tag_file" : 'tags.data'
+    "template_path": os.path.join(os.path.dirname(__file__), "templates"),
+    "csearch_path": '/u01/cdata',
+    "csearch_tag_file": 'tags.data'
 }
 
 application = tornado.web.Application([
-	(r"/", IndexHandler),
-	(r"/index", IndexHandler),
-	(r"/add", AddHandler),	
-	(r"/file", FileHandler)
+    (r"/", IndexHandler),
+    (r"/index", IndexHandler),
+    (r"/add", AddHandler),
+    (r"/file", FileHandler)
 ], **settings)
 if __name__ == "__main__":
-	application.listen(80)
-	tornado.ioloop.IOLoop.instance().start()
-
+    application.listen(80)
+    tornado.ioloop.IOLoop.instance().start()
